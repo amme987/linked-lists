af5b6798b8b13838344fa4ac3dbdc777caa918d9
amme987 <emmagination97@gmail.com>
2024-07-10 08:58:44 -0400

    Build 'prepend' and 'head' functions

--- linkedList.js
+++ linkedList.js
@@ -10,21 +10,25 @@ class LinkedList {
     if (this.firstNode === null) {
       this.firstNode = new Node(value);
     } else {
-      let tmp = this.firstNode;
-      while (tmp.nextNode !== null) {
-        tmp = tmp.nextNode;
+      let temp = this.firstNode;
+      while (temp.nextNode !== null) {
+        temp = temp.nextNode;
       }
-      tmp.nextNode = new Node(value);
+      temp.nextNode = new Node(value);
     }
   }
 
   // adds a new node containing value to the start of the list
   prepend(value) {
-    let node = new Node(value);
-    node.nextNode = this.head;
-    // console.log(this.head);
-    this.head = node.value;
-    // console.log(node.nextNode);
+    if (this.firstNode === null) {
+      this.firstNode = new Node(value);
+    } else {
+      let temp = new Node(value);
+      temp.nextNode = this.firstNode;
+      // console.log(this.head);
+      this.firstNode = temp;
+      // console.log(node.nextNode);
+    }
   }
 
   // returns the total number of nodes in the list
@@ -66,7 +70,9 @@ let test = new LinkedList();
 test.append('why');
 // console.log(test);
 
-test.append('what');
+test.prepend('what');
+// console.log(test.head());
 test.append('how');
+test.prepend('first');
 console.log(JSON.stringify(test));
 // console.log(test.prepend('ugh'));
